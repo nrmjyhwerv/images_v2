39803edceaa82e9e17052df9382375c3b53e3542
nrmjyhwerv <klusstycat@gmail.com>
2025-06-22 11:48:20 +0200

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,1 +1,16 @@
-print("working")
+import os
+import time
+from colorama import init, Fore, Style
+
+# Initialize colorama
+init(autoreset=True)
+
+# Clear console
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+clear_console()
+
+# Print message in color
+print(Fore.CYAN + Style.BRIGHT + "Thanks for using Cryptalis Panel")
+
